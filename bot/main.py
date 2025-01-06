5a85344559978c4fa28e6ee7731944b3a1aba0c5
antoo69 <antobarry69@gmail.com>
2025-01-06 17:29:55 +0700

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,14 @@
 from pyrogram import Client
 from bot.modules import tag_module
 from start import start_message
+from config import API_ID, API_HASH, BOT_TOKEN
 
 def run_bot():
     app = Client(
         "my_bot",
-        api_id="YOUR_API_ID",  # Ganti dengan API ID Anda
-        api_hash="YOUR_API_HASH",  # Ganti dengan API Hash Anda
-        bot_token="YOUR_BOT_TOKEN"  # Ganti dengan token bot Anda
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN
     )
 
     # Tambahkan handler
